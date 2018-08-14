c2f79fa00d32f43e8244f36280748ea87f2694c0
Alex <alxmer111@gmail.com>
2018-08-15 03:19:20 +0800

    webpack dev server and watch

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,11 @@ module.exports = {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
-	plugins: [,
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'Webpack App',
